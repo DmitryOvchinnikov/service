5c6482fff21649b00158529046a68b7347a934a4
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2022-04-24 15:55:35 +0300

    Logger, config added.

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,35 +1,155 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"expvar"
 	"fmt"
-	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
+	"time"
 
+	"github.com/dmitryovchinnikov/service/foundation/logger"
 	"go.uber.org/automaxprocs/maxprocs"
+
+	conf "github.com/ardanlabs/conf/v3"
+
+	"go.uber.org/zap"
 )
 
+/*
+	todo: Need to figure out timeouts for http service.
+*/
+
+// build is the git version of this program. It is set using build flags in the makefile.
 var build = "develop"
 
 func main() {
-	// GOMAXPROCS
-	// Set the correct number of threads for the service
-	// based on what is available either by the machine or quotas.
-	if _, err := maxprocs.Set(); err != nil {
-		fmt.Printf("maxprocs: %w", err)
+
+	// Construct the application logger.
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer log.Sync()
 
-	g := runtime.GOMAXPROCS(0)
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
 
-	log.Printf("starting service build[%s], CPU[%d]", build, g)
-	defer log.Println("service ended")
+func run(log *zap.SugaredLogger) error {
 
+	// GOMAXPROCS
+
+	// Want to see what maxprocs reports.
+	opt := maxprocs.Logger(log.Infof)
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(opt); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"` // noprint, mask to invisible
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "copyright information here",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// App Starting
+
+	log.Infow("starting service", "version", build)
+	defer log.Infow("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Infow("startup", "config", out)
+
+	expvar.NewString("build").Set(build)
+
+	// Start API Service
+
+	log.Infow("startup", "status", "initializing V1 API support")
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
 
-	log.Println("stopping service")
+	// Construct a server to service the requests against the mux.
+	api := http.Server{
+		Addr:         cfg.Web.APIHost,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     zap.NewStdLog(log.Desugar()),
+	}
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	// Start the service listening for api requests.
+	go func() {
+		log.Infow("startup", "status", "api router started", "host", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// Shutdown
+
+	// Blocking main and waiting for shutdown.
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+		// Give outstanding requests a deadline for completion.
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		// Asking listener to shut down and shed load.
+		if err := api.Shutdown(ctx); err != nil {
+			api.Close()
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/dmitryovchinnikov/service
 
 go 1.17
 
-require go.uber.org/automaxprocs v1.5.1
+require (
+	github.com/ardanlabs/conf/v3 v3.1.2
+	go.uber.org/automaxprocs v1.5.1
+	go.uber.org/zap v1.21.0
+)
+
+require (
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+)
